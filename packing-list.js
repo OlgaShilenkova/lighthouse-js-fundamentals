53ce29cbbd47b4da68adb4103beaa6482d0a7a22
Olga Shilenkova <Olga1shilenkova@gmail.com>
2021-11-01 12:22:49 -0600

    Changw to itteraye with while loop

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,19 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
+/*
+with FOR LOOP
 console.log("Kitchen staff to pack");
 for ( let i = 0; i<packingList.length; i++){
   console.log(packingList[i]);
+}
+*/
+
+
+// with while loop
+console.log("Kitchen staff to pack:")
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  // keep this i++ inside {}, otherwise indefinite loop happen
+  i++;
 }
